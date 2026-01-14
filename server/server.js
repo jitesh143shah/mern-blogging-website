ff103dd80cdd53b923c133887352c4295fdd3add
jitesh143shah <jitesh143shah@gmail.com>
2026-01-14 23:07:37 +0545

    server setup is done today

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+const server = express();
+let PORT = 5000;
+
+server.use(express.json());
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+});
+
+server.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Sign in is working", body: req.body });
+});
+
+server.listen(PORT, () => {
+  console.log("Listening on port --> " + PORT);
+});
